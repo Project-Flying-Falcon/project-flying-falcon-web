86ca9a351b64940930cc0ced4feb7bdecb4822be
Vikas Duvvuri <vikas.k.duvvuri@gmail.com>
2026-03-02 01:39:35 -0500

    Order Feature

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -3,27 +3,7 @@ import data from "./data";
 function Products() {
   return (
     <div className="content">
-      <ul className="products">
-        {data.products.map((product) => (
-          <li key={product.name}>
-            <div className="product">
-              <img
-                className="product-image"
-                src={product.imageUrl}
-                alt="product"
-              />
-              <div className="product-name">
-                <a href="product.html">{product.name}</a>
-              </div>
-              <div className="product-brand">{product.brand}</div>
-              <div className="product-price">${product.price}</div>
-              <div className="product-rating">
-                {product.rating} Stars ({product.numberOfReviews} Reviews)
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <ul className="products"></ul>
     </div>
   );
 }
