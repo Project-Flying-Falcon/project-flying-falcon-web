5acba932e0dc96b4f35b823afdbdaff1a686dc1c
quinnmoore54 <quinnmoore54@gmail.com>
2026-04-26 18:39:20 -0400

    Through page 4 (part 1)

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,4 +1,22 @@
-import data from "./data";
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+
+type Product = {
+    id: number;
+    name: string;
+    description: string;
+    brand: string;
+    price: number;
+    rating: number;
+    numberOfReviews: number;
+    imageUrl: string;
+}
+
+const emptyProducts: Product [] = [];
+
 
 function Products() {
   return (
@@ -6,6 +24,20 @@ function Products() {
       <ul className="products"></ul>
     </div>
   );
+
+const [products, setProducts]: [Product[], (products: Product[]) => void] = useState(emptyProducts);
+
+  useEffect(()=> {
+    axios.get<Product[]>("http://localhost:5500/catalog", 
+        {
+            headers: {
+                "Content-Type":"application/json",
+            },
+        })
+        .then((response)=>setProducts(response.data))
+        .catch((error) => console.log(error));
+        }, []);
+
 }
 
 export default Products;
